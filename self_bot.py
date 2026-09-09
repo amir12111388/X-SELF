2bdfeda182ccc2a90fffe655f74f86d5310dacf4
amir12111388 <mohamad.1.bananas.com@gmail.com>
2026-09-09 23:05:38 +0330

    Update self_bot.py

--- self_bot.py
+++ self_bot.py
@@ -592,7 +592,7 @@ class TelegramAccount:
                 "• `راهنما` — راهنمای سریع\n"
                 "• `وضعیت` — وضعیت اجرای سلف\n"
                 "• `پنل` — همه تنظیمات و امکانات\n\n"
-                f"🔮 ارائه‌شده توسط {self.brand_username('brand_powered_by')}"
+                f"🔮 ارائه‌شده توسط {self.brand_username(''https://t.me/SELF_BOT_FPS)}"
             )
             await self.queued_send_message(
                 "me",
